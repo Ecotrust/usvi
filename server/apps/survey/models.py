2e450462100ecfe5674a8f19335eaa0d95d55e20
Tim Glaser <tim.glaser@ecotrust.org>
2013-12-23 10:09:06 -0800

    Fix for multi-select exception clause.

--- server/apps/survey/models.py
+++ server/apps/survey/models.py
@@ -418,7 +418,7 @@ class Response(caching.base.CachingMixin, models.Model):
                                         row_label=answer['label'], row_text=answer['text'],
                                         col_label=grid_col.label, col_text=grid_col.text)
                                     grid_answer.save()
-                            except:
+                            except  Exception as e:
                                 print "problem with ", answer
                                 print e
                         else:
