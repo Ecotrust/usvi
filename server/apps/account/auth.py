091d6bccccac36f0848325796038ac0e41bc4688
Edwin Knuth <edwin@pointnineseven.com>
2014-01-06 11:55:50 -0800

    slight change

--- server/apps/account/auth.py
+++ server/apps/account/auth.py
@@ -1,14 +1,15 @@
 from social.backends.open_id import OpenIdAuth
-from social.exceptions import AuthMissingParameter
 
 
 class OSTOpenId(OpenIdAuth):
     """OST OpenID authentication backend"""
     name = 'ost'
     URL = 'http://oceanspaces.org/user'
+
     def get_user_details(self, response):
         """Return user details from OST account"""
         email = self.data.get('openid.sreg.email')
+
         return {'username': email,
                 'email': email,
                 'fullname': self.data.get('openid.sreg.nickname')}
