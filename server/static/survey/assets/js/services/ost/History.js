4b53ba612a7ae28e1c29637bda7bf6daa862faf2
Tim Glaser <tim.glaser@ecotrust.org>
2014-01-08 13:55:22 -0800

    Display survey answers in survey history.

--- server/static/survey/assets/js/services/ost/History.js
+++ server/static/survey/assets/js/services/ost/History.js
@@ -2,18 +2,6 @@
 
 angular.module('askApp')
   .factory('history', function ($http, $location) {
-    // Service logic
-    // ...
-
-    // var survey, 
-    //     page,
-    //     answers;
-
-    // var initializeSurvey = function(thisSurvey, thisPage, thisAnswers) {
-    //     survey = thisSurvey;
-    //     page = thisPage;
-    //     answers = thisAnswers;
-    // };
 
     var getSurveyTitle = function(respondent) {
         var title = respondent.survey;
@@ -21,45 +9,35 @@ angular.module('askApp')
         return title;
     }
 
-    var getTitle = function(respondent) {
+    var getOrganizationName = function(respondent) {
+        var val = "None";
         try {
-            var island = _.findWhere(respondent.responses, {question: 'island'}).answer.text,
-                title = 'USVI Commercial Catch Report Form - ' + island;  
-        } catch(e) {
-            var title = 'USVI Commercial Catch Report Form';
-        }
-        return title;
-    };
-
-    var gearTypeIncludes = function(type, respondent) {
-        try { 
-            var gearTypes = _.pluck(_.findWhere(respondent.responses, {question: 'gear-type'}).answer, 'label');
-
-            if ( gearTypes.join().indexOf(type) !== -1 ) {
-                return true;
-            } 
-        } catch(e) {
-            return '';
-        }
-    };
-
-    var trapTypeIncludes = function(type, respondent) {
-        try { 
-            var gearTypes = _.pluck(_.findWhere(respondent.responses, {question: 'gear-type'}).answer, 'label');  
-            if ( gearTypes.join().indexOf(type) !== -1 ) {
-                return true;
-            } 
-        } catch(e) {
-            return '';
-        }
+            val = _.findWhere(respondent.responses, {question: 'org-name'}).answer.text;
+        } catch(e) { }
+        return val;
     };
 
     var getAnswer = function(questionSlug, respondent) {
+        var answer = '';
         try {
-            if (questionSlug === 'weight-line-or-reel' || questionSlug === 'weight-traps' || questionSlug === 'weight-nets' || questionSlug === 'weight-spear-or-by-hand') {
+
+            if (0 <= ['org-type', 'org-mpa-funded', 'proj-has-sufficient-funds', 'proj-data-availability'].indexOf(questionSlug)) {
+                // For single-select, yes-no
+                answer = _.findWhere(respondent.responses, {question: questionSlug}).answer.text;
+
+            } else if (0 <= ['org-funding', 'ecosystem-features'].indexOf(questionSlug)) {
+                // For multi-select
+                var objs = _.findWhere(respondent.responses, {question: questionSlug}).answer;
+                _.each(objs, function(obj, index) {
+                    answer += answer.length > 0 ? ", " + obj.text : obj.text;
+                });
+
+            } else if (0 <= ['org-funding'].indexOf(questionSlug)) {
+                // Grouped multi-select.
                 var island = _.findWhere(respondent.responses, {question: 'island'}).answer.label,
-                    islandSlug = (island === 'st-thomas' || island === 'st-john') ? 'st-thomas-st-john' : island,
-                    answer = _.findWhere(respondent.responses, {question: questionSlug + '-' + islandSlug}).answer;
+                    islandSlug = (island === 'st-thomas' || island === 'st-john') ? 'st-thomas-st-john' : island;
+
+                answer = _.findWhere(respondent.responses, {question: questionSlug + '-' + islandSlug}).answer;
 
                 // TODO: currently no way of determining definitively whether answers are from a grouped multi-select or an ungrouped multi-select
                 // (no groupName may present because either just Others were selected, or no groups were present)
@@ -75,48 +53,14 @@ angular.module('askApp')
                         obj.showGroupName = undefined;
                     }
                 });
-            } else if (questionSlug === 'trip-landing-site') {
-                // var island = _.findWhere(respondent.responses, {question: 'island'}).answer.label,
-                //     answer = _.findWhere(respondent.responses, {question: questionSlug + '-' + island}).answer;       
-                var answer = _.findWhere(respondent.responses, {question: questionSlug}).answer; 
-            } else if (questionSlug === 'days-soaked-lobster-traps') {                    
-                var unit = _.findWhere(respondent.responses, {question: 'time-soaked-lobster-traps'}).answer.unit,
-                    value = _.findWhere(respondent.responses, {question: 'time-soaked-lobster-traps'}).answer.value;
-                if (unit.toLowerCase().trim() === 'days') {
-                    return value;
-                } else {
-                    return  Math.floor(value / 24);
-                }
-            } else if (questionSlug === 'hours-soaked-lobster-traps') {                    
-                var unit = _.findWhere(respondent.responses, {question: 'time-soaked-lobster-traps'}).answer.unit,
-                    value = _.findWhere(respondent.responses, {question: 'time-soaked-lobster-traps'}).answer.value;
-                if (unit.toLowerCase().trim() === 'hours') {
-                    return value % 24;
-                } else {
-                    return  0;
-                }
-            } else if (questionSlug === 'days-soaked-fish-traps') {                    
-                var unit = _.findWhere(respondent.responses, {question: 'time-soaked-fish-traps'}).answer.unit,
-                    value = _.findWhere(respondent.responses, {question: 'time-soaked-fish-traps'}).answer.value;
-                if (unit.toLowerCase().trim() === 'days') {
-                    return value;
-                } else {
-                    return  Math.floor(value / 24);
-                }
-            } else if (questionSlug === 'hours-soaked-fish-traps') {                    
-                var unit = _.findWhere(respondent.responses, {question: 'time-soaked-fish-traps'}).answer.unit,
-                    value = _.findWhere(respondent.responses, {question: 'time-soaked-fish-traps'}).answer.value;
-                if (unit.toLowerCase().trim() === 'hours') {
-                    return value % 24;
-                } else {
-                    return  0;
-                }
+
             } else {
-                var answer = _.findWhere(respondent.responses, {question: questionSlug}).answer;
+                answer = _.findWhere(respondent.responses, {question: questionSlug}).answer;
+
             }
             
         } catch(e) {
-            var answer = '';
+            answer = '';
         }
         if (answer === 'NA') {
             answer = '';
@@ -129,9 +73,7 @@ angular.module('askApp')
 
     // Public API here
     return {
-      'getTitle': getTitle,
-      'gearTypeIncludes': gearTypeIncludes,
-      'trapTypeIncludes': trapTypeIncludes,
+      'getOrganizationName': getOrganizationName,
       'getAnswer': getAnswer
     };
   });
