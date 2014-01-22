a7383288cc41c1e8db30119fade1f1612ca29f06
Edwin Knuth <edwin@pointnineseven.com>
2014-01-21 16:34:48 -0800

    other conflicts?

--- server/static/survey/assets/js/controllers/splash.js
+++ server/static/survey/assets/js/controllers/splash.js
@@ -3,9 +3,6 @@ angular.module('askApp')
   .controller('SplashCtrl', function ($scope, $location, $http) {
   	$scope.version = app.version;
   	$scope.stage = app.stage;
-<<<<<<< HEAD
-  	$('.splash').height($('body').height()).backstretch(app.viewPath + 'assets/img/splash.png');
-=======
+
   	// $('.splash').height($('body').height()).backstretch('assets/img/splash.png');
->>>>>>> gs/master
   });
