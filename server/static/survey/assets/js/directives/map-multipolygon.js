61e278ce350239fad866e2e8a2862060cdf75e95
Tim Glaser <tim.glaser@ecotrust.org>
2014-02-13 15:40:12 -0800

    Select All feature for polygon map.

--- server/static/survey/assets/js/directives/map-multipolygon.js
+++ server/static/survey/assets/js/directives/map-multipolygon.js
@@ -1,7 +1,7 @@
 angular.module('askApp')
     .directive('map', function($http) {
         return {
-            template: '<div class="map ost-map"></div>',
+            templateUrl: app.viewPath + 'views/questionMapPolygons.html',
             restrict: 'EA',
             replace: true,
             transclude: true,
@@ -9,6 +9,7 @@ angular.module('askApp')
                 question: "=question" //scope.question.geojson, scope.question.zoom, etc
             },
             link: function(scope, element) {
+                scope.allPloygons = [];
                 if (scope.question.answer) {
 
                     //debugger;
@@ -19,6 +20,7 @@ angular.module('askApp')
 
                 $(".block-title").hide();
                 $(".question-title").addClass('map-question-title');
+                $("body").addClass('map-question');
 
                 // Layer init
                 var nautical = L.tileLayer.wms("http://egisws02.nos.noaa.gov/ArcGIS/services/RNC/NOAA_RNC/ImageServer/WMSServer", {
@@ -53,14 +55,14 @@ angular.module('askApp')
                 var options = { position: 'bottomleft' };
                 L.control.layers(baseMaps, null, options).addTo(map);
 
-                var layerClick = function(layer) {
+                var layerClick = function(layer, isSelectAll) {
                     var id = layer.feature.properties.ID;
                     if (layer.options.fillOpacity === 0) {                                  
                         layer.setStyle( {
                             fillOpacity: .6
                         });
                         scope.question.answer.push(id);
-                    } else {
+                    } else if (!isSelectAll) {
                         layer.setStyle( {
                             fillOpacity: 0
                         });
@@ -86,15 +88,43 @@ angular.module('askApp')
                                 });
                             }
                             layer.on("click", function (e) {
-                                layerClick(layer);
+                                layerClick(layer, false);
                             });
-
+                            scope.allPloygons.push(layer);
                         }
                     });
                     geojsonLayer.addTo(map);
                 });
                 
                 $('.leaflet-label').removeClass('leaflet-label-right');
+
+                
+                scope.selectAllPolygons = function () {
+                    _.each(scope.allPloygons, function (layer) {
+                        layerClick(layer, true);
+                    });
+                };
+
+
+                scope.hoverLatLng = {};
+
+                scope.onMouseMove = function (e /*Leaflet MouseEvent */) {
+                    scope.$apply(function (scope) {
+                        scope.hoverLatLng = e.latlng;
+                        $('.floatingMouseCoordinates').css('top', e.containerPoint.y + 15);
+                        $('.floatingMouseCoordinates').css('left', e.containerPoint.x + 15);
+                    });
+                };
+
+                scope.onMouseOut = function (e /*Leaflet MouseEvent */) {
+                    scope.$apply(function (scope) {
+                        scope.hoverLatLng = null;
+                    });
+                };
+
+                map.on('mousemove', scope.onMouseMove);
+                map.on('mouseout', scope.onMouseOut);
+
             }
         }
     });
