920051e5f51d4d5d89feda24caa8336779cbac4b
Tim Glaser <tim.glaser@ecotrust.org>
2014-03-11 13:25:35 -0700

    Sort, unique and remove HTML tags for dash-filter options.

--- server/static/survey/assets/js/directives/dash-filter.js
+++ server/static/survey/assets/js/directives/dash-filter.js
@@ -1,3 +1,4 @@
+
 angular.module('askApp')
     .directive('dashFilter', function($http, dashData) {
     return {
@@ -19,13 +20,41 @@ angular.module('askApp')
 
 
             function setFilterOptions () {
+                
+                var removeHtml = function (html) {
+                    var regex = /(<([^>]+)>)/ig,
+                        noHtml = html.replace(regex, "");
+                    return noHtml;
+                };
+
                 var onFail = function () { debugger; };
 
                 var onSuccess = function (data) {
-                    scope.filterOptions = _.pluck(data.answer_domain, "answer_text");
+                    // Grab the answer_text value for each item, sort 
+                    // them all and remove duplicates.
+                    var options = [],
+                        vals = _.pluck(data.answer_domain, "answer_text");
+                    vals = _.sortBy(vals, function(item){
+                        // Compare by the value itself.
+                        return item;
+                    });
+                    vals = _.uniq(vals, true);
+
+
+                    // Provide a displayLabel verstion that doesn't have
+                    // any html formatting.
+                    angular.forEach(vals, function(val) {
+                        options.push({
+                            displayLabel: removeHtml(val),
+                            value: val
+                        });
+                    });
+
+                    scope.filterOptions = options;
                 };
 
-                dashData.getDistribution(scope.surveySlug, scope.questionSlug, '' /*no filter*/, onSuccess, onFail);
+                dashData.getDistribution(scope.surveySlug, scope.questionSlug,
+                    '' /*no filter*/, onSuccess, onFail);
             }
 
 
