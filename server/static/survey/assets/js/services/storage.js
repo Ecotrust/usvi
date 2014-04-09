ecc7f5e30524d714626d692d8b62168921068e34
Edwin Knuth <edwin@pointnineseven.com>
2014-04-09 10:05:13 -0700

    linting

--- server/static/survey/assets/js/services/storage.js
+++ server/static/survey/assets/js/services/storage.js
@@ -1,45 +1,48 @@
-angular.module('askApp').service('storage', function() {
+(function () {
+    'use strict';
+    angular.module('askApp').service('storage', function() {
 
-    var saveState = function (state) {
-        localStorage.setItem('hapifis-' + state.user.username, JSON.stringify(state));
-        localStorage.setItem('hapifis', JSON.stringify({ currentUser: state.user.username }));
-    };
+        var saveState = function (state) {
+            localStorage.setItem('hapifis-' + state.user.username, JSON.stringify(state));
+            localStorage.setItem('hapifis', JSON.stringify({ currentUser: state.user.username }));
+        };
 
-    var getState = function (username) {
-        return JSON.parse(localStorage.getItem('hapifis-' + username));
-    };
+        var getState = function (username) {
+            return JSON.parse(localStorage.getItem('hapifis-' + username));
+        };
 
-    var clearCache = function () {
-        var keys = Object.keys(localStorage);
-        _.each(keys, function (key) {
-            if (/^hapifi/.test(key)) {
-                localStorage.removeItem(key);
-            }
-        });
-    };
+        var clearCache = function () {
+            var keys = Object.keys(localStorage);
+            _.each(keys, function (key) {
+                if (/^hapifi/.test(key)) {
+                    localStorage.removeItem(key);
+                }
+            });
+        };
 
-    var getCurrentUser = function () {
-        return JSON.parse(localStorage.getItem('hapifis')).currentUser;
-    };
+        var getCurrentUser = function () {
+            return JSON.parse(localStorage.getItem('hapifis')).currentUser;
+        };
 
-    var getStateForUser = function (username) {
-        return JSON.parse(localStorage.getItem('hapifis-' + username));
-    };
+        var getStateForUser = function (username) {
+            return JSON.parse(localStorage.getItem('hapifis-' + username));
+        };
 
-     var cleanUserObject = function(user){
-        // Mainly used to parse the password to a string before sending to the server
-        var password = user.password;
-        user.password = ''+password;
-        return user;
-    };
+        var cleanUserObject = function(user){
+            // Mainly used to parse the password to a string before sending to the server
+            var password = user.password;
+            user.password = ''+password;
+            return user;
+        };
 
-    return {
-        saveState: saveState,
-        getState: getState,
-        clearCache: clearCache,
-        getCurrentUser: getCurrentUser,
-        getStateForUser: getStateForUser,
-        cleanUserObject : cleanUserObject,
+        return {
+            saveState: saveState,
+            getState: getState,
+            clearCache: clearCache,
+            getCurrentUser: getCurrentUser,
+            getStateForUser: getStateForUser,
+            cleanUserObject : cleanUserObject,
 
-    };
-});
+        };
+    });
+})();
