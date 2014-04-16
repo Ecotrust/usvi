8abe8eaa25c0041268989c3fa6a87204d215b811
Edwin Knuth <edwin@pointnineseven.com>
2014-04-16 12:22:06 -0700

    labeling unmatched fish in fisher mobile summary for #263

--- server/static/survey/assets/js/controllers/summary.js
+++ server/static/survey/assets/js/controllers/summary.js
@@ -1,16 +1,16 @@
+(function () {
+    'use strict';
 
-angular.module('askApp')
-    .controller('SummaryCtrl', function($scope, $http, $routeParams) {
-    	var url = app.server + '/reports/distribution/catch-report/weight-*?fisher=true';
+    angular.module('askApp')
+        .controller('SummaryCtrl', function($scope, $http) {
+            var url = app.server + '/reports/distribution/catch-report/weight-*?fisher=true';
 
-    	$http.get(url)
-    		.success(function (data) {
-    			$scope.summary = data.results.length ? data.results : 'none';
-    			$scope.max = _.max($scope.summary, function (item) {
-    				return item.total 
-    			}).total;
-    		})
-    		.error(function (err) {
-    			debugger;
-    		});
-    });
+            $http.get(url)
+                .success(function (data) {
+                    $scope.summary = data.results.length ? data.results : 'none';
+                    $scope.max = _.max($scope.summary, function (item) {
+                        return item.total;
+                    }).total;
+                });
+        });
+})();
