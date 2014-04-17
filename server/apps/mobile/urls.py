a45a194c2d9e7a93d306c5193c5d73677603185d
Edwin Knuth <edwin@pointnineseven.com>
2014-04-17 10:38:54 -0700

    getVersion, too!

--- server/apps/mobile/urls.py
+++ server/apps/mobile/urls.py
@@ -4,4 +4,5 @@ from views import *
 
 urlpatterns = patterns('',
                        (r'^getMessages', getMessages),
+                       (r'^getVersion', getMessages),
                        )
